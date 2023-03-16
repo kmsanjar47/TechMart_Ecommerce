27218ad280b4063cd8e0d1aa941f4902e3603d7c
kmsanjar47 <khan.md.saifullah@g.bracu.ac.bd>
2023-03-16 16:46:27 +0600

    .

--- lib/pages/order_completed_page.dart
+++ lib/pages/order_completed_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:e_commerce_app/pages/navigation_page.dart';
 import 'package:e_commerce_app/widgets/title_text.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
@@ -11,9 +14,16 @@ class OrderCompletedPage extends StatefulWidget {
 
 class _OrderCompletedPageState extends State<OrderCompletedPage> with TickerProviderStateMixin{
   late AnimationController animationCtl;
+  
+  redirectToHomepage(){
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>NavigationPage()), (route) => false);
+    });
+  }
   @override
   void initState() {
     super.initState();
+    redirectToHomepage();
     animationCtl = AnimationController(vsync: this);
   }
   @override
